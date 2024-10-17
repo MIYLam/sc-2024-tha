d630ddc4158498a27cfb5177f1fabcee30022a55
Maurice Lam <mlam7947@uni.sydney.edu.au>
2024-10-17 12:27:48 +1100

    tests for orgid and children

--- DOCS.md
+++ DOCS.md
@@ -1,15 +1,53 @@
 # Notes for self 
 
 - The f(*driver) is used to access the needed functions
-- The filepaths that are printed at the start are used by the get sample data function, don't need to touch. 
+- The filepaths that are printed at the start are used by the get sample data function, don't need to touch.
+- The more I work on this the more I am realising that go standard should be camel case but I want to get this done ASAP (sorry) 
+- Program will execute based on first instance of the directory named in the input
+    - I don't think I have the capacity to try and fix the database and clear it of duplicates
 
-# Main approach
+# Approach explantions
+## Get all children
+### High level steps
 - Find all files in an organisation 
 - Find all the ones with the target directory + something after it
 - Print that out
-## Reasoning:
+- Runtime: O(n), as we only need to iterate through the whole database once.
+    - Should check every file as there is no gurantee that the database is sorted
+### Reasoning:
 - Assuming a correct tree structure, every child/grandchild will have the target directory as a parent.
     - Thus, the parent always will appear in the path before. 
+### Edge cases:
+- Duplicate folder names in the same org, especially with both being in different levels of the tree (such as concise cable in the sample data)
+    - Solution: Continue to print out children of both
+    - Reasoning: Still achieves what is being asked on the task
+        - In the case that the duplicates are parent/child, neither will be printed when listing the children 
+        - Purpose of the function isn't to fix the database having duplicate names
+
+
+## Move folder 
+### High level steps 
+- Find file to change and its children and parent
+    - If they don't exist then return error
+- Find new parent
+    - Error if it doesn't exist
+- Do checks 
+    - Second folder is not a child of the first 
+    - Different orgID 
+- Make the required file path changes 
+    - Remove from original parent 
+
+
+### Reasonings 
+
+### Edge cases
+
+# Testing
+- Used the testify module as it felt more familiar with testing done before 
+- All the data is in the tesitng file itself, admittedly could do better 
+
+
+
 # Progress track by commit
 ## f86913
 - Never used Go before, so needed some time to look through the structure of the package
--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -2,28 +2,71 @@ package folder_test
 
 import (
 	"testing"
-
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
-	// "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
-// feel free to change how the unit test is structured
-func Test_folder_GetFoldersByOrgID(t *testing.T) {
-	t.Parallel()
-	tests := [...]struct {
+var def_org_id = uuid.FromStringOrNil(folder.DefaultOrgID)
+
+var sampleData = []folder.Folder{
+		{"creative-scalphunter",  def_org_id,  "creative-scalphunter"},
+		{"clear-arclight", def_org_id,  "creative-scalphunter.clear-arclight"},
+		{"bursting-lionheart", def_org_id,  "creative-scalphunter.clear-arclight.bursting-lionheart"},
+		{"noble-vixen",  uuid.FromStringOrNil("somethingrandom"),  "noble-vixen"},
+	}
+var GetFoldersByOrgID_tests = [...]struct {
 		name    string
 		orgID   uuid.UUID
 		folders []folder.Folder
 		want    []folder.Folder
 	}{
-		// TODO: your tests here
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// f := folder.NewDriver(tt.folders)
-			// get := f.GetFoldersByOrgID(tt.orgID)
+		{"incorrect orgID", def_org_id, sampleData, []folder.Folder{
+				{"creative-scalphunter",  def_org_id,  "creative-scalphunter"},
+				{"clear-arclight", def_org_id,  "creative-scalphunter.clear-arclight"},
+				{"bursting-lionheart", def_org_id,  "creative-scalphunter.clear-arclight.bursting-lionheart"},
 
+			},
+		},
+
+	}
+var GetAllChildFolders_tests = [...]struct {
+		name    string
+		fname 	string
+		orgID   uuid.UUID
+		folders []folder.Folder
+		want    []folder.Folder
+	}{
+		{"normal case", "creative-scalphunter", def_org_id, sampleData, []folder.Folder{
+				{"clear-arclight", def_org_id,  "creative-scalphunter.clear-arclight"},
+				{"bursting-lionheart", def_org_id,  "creative-scalphunter.clear-arclight.bursting-lionheart"},
+
+			},
+		},
+
+	}
+// feel free to change how the unit test is structured
+func Test_folder_GetFoldersByOrgID(t *testing.T) {
+	t.Parallel()
+	for _, tt := range GetFoldersByOrgID_tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			f := folder.NewDriver(tt.folders)
+			get := f.GetFoldersByOrgID(tt.orgID)
+			assert.Equal(get, tt.want)
+			
+		})
+	}
+}
+func Test_folder_GetAllChildFolders(t *testing.T) {
+	t.Parallel()
+	for _, tt := range GetAllChildFolders_tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			f := folder.NewDriver(tt.folders)
+			get := f.GetAllChildFolders(def_org_id, tt.fname)
+			assert.Equal(get, tt.want)
+			
 		})
 	}
 }
